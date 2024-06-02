4c143c28a4a3af34b4d04f6f78092b65f7399fdf
tovstolug <daniil.tovstolug@gmail.com>
2024-06-02 22:24:34 +0300

    add transaction sending

--- scripts/metamask.js
+++ scripts/metamask.js
@@ -1,18 +1,82 @@
+let accounts
+
+function connect() {
+    if (!window.ethereum) {
+        console.log("metamask not found")
+        return;
+    }
+
+    window.ethereum.request({ method: "eth_requestAccounts" })
+        .then(res => {
+            accounts = res
+        }).catch(err => {
+            console.log(err)
+        })
+
+    window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [
+            {
+                "chainId": "0xAA36A7" // 11155111
+            }
+        ]
+    })
+
+    const statusDiv = document.getElementById("metamask-status");
+    statusDiv.textContent = 'MetaMask wallet connected'
+}
+
+function addLocalChain() {
+    window.ethereum
+        .request({
+            method: 'wallet_addEthereumChain',
+            params: [
+                {
+                    "nativeCurrency": {
+                        "name": "HETH",
+                        "symbol": "HETH",
+                        "decimals": 18
+                    },
+                    "rpcUrls": [
+                        "http://127.0.0.1:8545"
+                    ],
+                    "chainId": "0x7A69",
+                    "chainName": "Hardhat Local"
+                },
+            ],
+        })
+        .then((res) => console.log('add', res))
+        .catch((e) => console.log('ADD ERR', e));
+}
+
 const transferButton = document.getElementById("transfer-eth");
 
 transferButton.addEventListener("click", () => {
-  window.ethereum.request({
-    method: "eth_sendTransaction",
-    params: [
-      {
-        to: "0x9F59a11B90556902DEf7e46de61D5db7F478ea05",
-        value: "0xDE0B6B3A7640000", // 1000000000000000000 wei or 0.1 eth
-      },
-    ]
-  }).then((result) => {
-    console.log(result);
-  })
-    .catch((error) => {
-      console.log(error);
+    const decodedDataElement = document.getElementById("decoded-data");
+    const txData = JSON.parse(decodedDataElement.textContent)
+
+    if (!txData) {
+        console.log("error parsing decoded tx data");
+        return
+    }
+
+    window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [
+            {
+                from: accounts[0],
+                to: txData['to'],
+                // value: "0xDE0B6B3A7640000", // 1000000000000000000 wei or 0.1 eth
+                data: txData['data'],
+                chainId: "11155111"
+            },
+        ]
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        alert("smth went wrong while sending transaction");
+        console.log(error);
     })
 });
+
+connect();
